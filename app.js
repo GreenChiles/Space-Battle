5d407e8a3b4682f9fe628c80c8641de3816d949b
GreenChiles <simslj98@gmail.com>
2022-01-12 19:11:48 -0700

    save

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-const myself = {
-    
+class AssignToMyself  {
+    constructor(healthPoints, firepower, accuracy){
+    this.healthPoints = 20
+    this.firepower = 5
+    this.accuracy = 0.7
+    }
 }
+
+const myself = new AssignToMyself()
+//console.log(myself)
+
+
+
+class AssignToAliens {
+    constructor(healthPoints, firepower, accuracy){
+        this.healthPoints =  Math.floor((Math.random() * 3) + 3) // gets a number between 0 and 3, then adds 3, thus gives random number between 3 - 6.
+        this.firepower = Math.floor((Math.random))
+    }
+}
+
+const alien1 = new AssignToAliens()
+console.log(alien1)
+
