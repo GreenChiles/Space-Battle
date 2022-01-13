2e440fc05f25108936b7fef2dce218a48ecdc298
GreenChiles <simslj98@gmail.com>
2022-01-12 20:48:55 -0700

    retreat method made

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ class AssignToMyself  {
     this.firepower = 5
     this.accuracy = 0.7
     }
+
     shoot(alien){
         if (Math.random() <= this.accuracy){
             alien.healthPoints -= this.firepower
@@ -12,6 +13,13 @@ class AssignToMyself  {
             console.log("miss, Lol")
         } else {
             console.log("missed?")
+        }
+    }
+
+    retreat(){ // check if this works when leave function is made
+        if (alien == dead){
+            leave()
+            console.log(" i quit")
         }
     }
     
@@ -27,6 +35,15 @@ class AssignToAliens {
         this.firepower = Math.floor((Math.random() * 2) + 2)
         this.accuracy = Math.round(Math.random() * (8 - 6) + 6) / 10
     }
+    shoot(me){
+        if (Math.random() <= this.accuracy){
+            me.healthPoints -= this.firepower
+            console.log("hit")
+        } else if (Math.random() > this.accuracy){
+            console.log("miss, Lol")
+        } else {
+            console.log("missed?")
+        }}
 }
 
 const alien1 = new AssignToAliens()
@@ -36,6 +53,10 @@ const alien4 = new AssignToAliens()
 const alien5 = new AssignToAliens()
 
 //console.log(alien1)
-myself.shoot(alien1)
+/*myself.shoot(alien1)
+console.log(myself)
+console.log(alien1)*/
+
+alien1.shoot(myself)
 console.log(myself)
 console.log(alien1)
